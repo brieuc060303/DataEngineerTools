19b3c91ae734a5a73ad33b3c667b8fcbcfd69bee
Brieuc Valence <brieuc.valence@edu.esiee.fr>
2023-11-29 12:25:37 +0100

    td1&td2

--- 2Scrapy/citations_churchill_spider1.py
+++ 2Scrapy/citations_churchill_spider1.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import re
 class ChurchillQuotesSpider(scrapy.Spider):
     name = "citations de Churchill"
     start_urls = ["http://evene.lefigaro.fr/citations/winston-churchill",]
@@ -7,4 +7,6 @@ class ChurchillQuotesSpider(scrapy.Spider):
     def parse(self, response):
         for cit in response.xpath('//div[@class="figsco__quote__text"]'):
             text_value = cit.xpath('a/text()').extract_first()
+            text_value = re.sub('”', "", text_value)
+            text_value = re.sub('“', "", text_value)
             yield { 'text' : text_value }
